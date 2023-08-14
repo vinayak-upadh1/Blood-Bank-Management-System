dd05e0ff000f126b334e894500bd68679dd54a7a
Vinayak Upadhyay <vinayakupadhyay42@gmail.com>
2023-08-14 17:30:22 +0530

    updated database

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
